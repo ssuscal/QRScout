1a589f5da748f75ec0a71b34cbd51cbd638c5d40
Steve Suscal <steve12s@icloud.com>
2024-04-25 16:49:58 -0400

    v1.0.5

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ssuscal.github.io/',
+  base: 'ssuscal.github.io/',
   plugins: [preact()],
 });
