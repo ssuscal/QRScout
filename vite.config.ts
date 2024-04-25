7997f13a7d8c8ec26103fa2186a55028dbd4a550
Steve Suscal <steve12s@icloud.com>
2024-04-25 15:55:28 -0400

    attempt fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout/',
+  base: '/ssuscal.github.io/',
   plugins: [preact()],
 });
